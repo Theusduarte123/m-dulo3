22ff6a56a2265f2015231af1495ad87d4f4d22f4
Theusduarte123 <113534773+Theusduarte123@users.noreply.github.com>
2022-11-22 08:02:23 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const port = process.env.PORT || 8080;
 
 server.use(middlewares);
 server.use(jsonServer.rewriter({
-  "/todos_os_empregados": "/empregados",
-  "/adicionar_empregado": "/empregados",
-  "/editar_empregado/:id": "/empregados/:id",
-  "/deletar_empregado/:id": "/empregados/:id"
+  "/todos_os_perificos": "/perifericos",
+  "/adicionar_periferico": "/perifericos",
+  "/editar_periferico/:id": "/perifericos/:id",
+  "/deletar_periferico/:id": "/perifericos/:id"
 }))
 server.use(router);
 server.listen(port, () => { console.log(`JSON Server está rodando na porta ${port}`) })
