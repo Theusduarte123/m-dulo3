48d4f70936b43786de6ceb023fee587fbfa0c4b1
Theusduarte123 <113534773+Theusduarte123@users.noreply.github.com>
2022-11-22 07:57:25 -0300

    att server

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-import jsonServer from 'json-server';
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); 
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200; 
+
+const port = process.env.PORT || 8080;
 
 server.use(middlewares);
+server.use(jsonServer.rewriter({
+  "/todos_os_empregados": "/empregados",
+  "/adicionar_empregado": "/empregados",
+  "/editar_empregado/:id": "/empregados/:id",
+  "/deletar_empregado/:id": "/empregados/:id"
+}))
 server.use(router);
-
-server.listen(port);
+server.listen(port, () => { console.log(`JSON Server está rodando na porta ${port}`) })
